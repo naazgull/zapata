b77c1efef0f47394c0e852785de30ab811488379
naazgull <pedro.figueiredo@gmail.com>
2014-06-07 16:51:07 +0100

    added OAuth2.0 support module
    Signed-off-by: naazgull <pedro.figueiredo@gmail.com>

--- cpp/zapata-core/conf/load.cpp
+++ cpp/zapata-core/conf/load.cpp
@@ -7,7 +7,23 @@ bool zapata::to_configuration(zapata::JSONObj& _out, string& _key_file_path) {
 		zapata::fromfile(_global_file, _out);
 	}
 
+	DIR *_dp = NULL;
+	struct dirent *_dirp = NULL;
 
+	if ((_dp = opendir("/etc/zapata/conf.d/")) != NULL) {
+		while ((_dirp = readdir(_dp)) != NULL) {
+			string _cname = string(_dirp->d_name);
+			if (_cname.find('.') != 0 && _dirp->d_type == DT_REG) {
+				_cname.insert(0, "/etc/zapata/conf.d/");
+				ifstream _confd_file;
+				_confd_file.open(_cname.data());
+				if (_confd_file.is_open()) {
+					zapata::fromfile(_confd_file, _out);
+				}
+			}
+		}
+		closedir(_dp);
+	}
 
 	ifstream _key_file;
 	_key_file.open(_key_file_path.data());
