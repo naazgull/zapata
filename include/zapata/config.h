1331ff93d847d14e70a155002949dc031479b4c7
naazgull <pedro.figueiredo@gmail.com>
2015-01-20 23:00:46 +0000

    added system V scripts to 'make install'

--- include/zapata/config.h
+++ include/zapata/config.h
@@ -25,9 +25,6 @@
 /* Define to 1 if you have the `z' library (-lz). */
 #define HAVE_LIBZ 1
 
-/* Define to 1 if you have the `zmq' library (-lzmq). */
-#define HAVE_LIBZMQ 1
-
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
 
