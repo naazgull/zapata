f2a31daf758ec390e9150b154704d296731eb6be
n@zgul <pedro.figueiredo@gmail.com>
2017-02-03 10:41:40 +0000

    generating python and lisp

---
+++ gen/src/zapata/gen/Handlers.lisp
@@ -0,0 +1,17 @@
+
+$[resource.handler.get]
+$[resource.handler.post]
+$[resource.handler.put]
+$[resource.handler.patch]
+$[resource.handler.delete]
+$[resource.handler.head]
+
+(zpt-on "$[resource.topic.regex]"
+    `(
+      $[resource.handler.get.name]
+      $[resource.handler.post.name]
+      $[resource.handler.put.name]
+      $[resource.handler.patch.name]
+      $[resource.handler.delete.name]
+      $[resource.handler.head.name]
+      ))
