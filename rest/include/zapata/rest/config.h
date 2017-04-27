5b35da36e327b7144c439a7d8a2405bdb175b565
n@zgul <pedro.figueiredo@gmail.com>
2017-04-27 14:15:11 +0100

    version bump

--- rest/include/zapata/rest/config.h
+++ rest/include/zapata/rest/config.h
@@ -91,7 +91,7 @@
 #define PACKAGE_NAME "zapata-rest"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "zapata-rest 0.9.6-1yakkety46"
+#define PACKAGE_STRING "zapata-rest 0.9.6-1yakkety47"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "zapata-rest"
@@ -100,10 +100,10 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.9.6-1yakkety46"
+#define PACKAGE_VERSION "0.9.6-1yakkety47"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.9.6-1yakkety46"
+#define VERSION "0.9.6-1yakkety47"
