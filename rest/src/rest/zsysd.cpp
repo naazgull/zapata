156ce4c08ed0c5f96eb045b22337d280cda4471c
n@zgul <pedro.figueiredo@gmail.com>
2017-05-03 18:41:40 +0100

    changes to project template

--- rest/src/rest/zsysd.cpp
+++ rest/src/rest/zsysd.cpp
@@ -38,36 +38,97 @@ using namespace __gnu_cxx;
 #endif
 
 int main(int argc, char* argv[]) {
-	/*
-	  [Unit]                                                                                                                                                                                                                                                                    
-	  Description=Myfox Container                                                                                                                                                                                                                                               
-	  After=docker.service                                                                                                                                                                                                                                                      
-	  Requires=docker.service                                                                                                                                                                                                                                                   
-                                                                                                                                                                                                                                                                          
-	  [Service]                                                                                                                                                                                                                                                                 
-	  TimeoutStartSec=0                                                                                                                                                                                                                                                         
-	  Restart=on-failure                                                                                                                                                                                                                                                        
-	  RemainAfterExit=true                                                                                                                                                                                                                                                      
-	  ExecStartPre=-/usr/bin/docker stop manager-myfox-contained                                                                                                                                                                                                                
-	  ExecStartPre=-/usr/bin/docker rm manager-myfox-contained                                                                                                                                                                                                                  
-	  ExecStart=/usr/bin/docker run -d --name manager-myfox-contained --net=host -P                                                                                                                                                                                             
-	  -v /var/log/zapata:/var/log/zapata -v /etc/muzzley/manager-myfox-contained:/etc                                                                                                                                                                                           
-	  /zapata/backend-enabled -v /home/projects/manager-myfox-contained:/usr/share/                                                                                                                                                                                             
-	  muzzley/manager-myfox-contained dockermuzz/managers:3.0.0                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                                                          
-	  ExecStop=/usr/bin/docker stop -t 2 manager-myfox-contained                                                                                                                                                                                                                
-	  ExecStopPost=/usr/bin/docker rm -f manager-myfox-contained                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                                          
-	  [Install]                                                                                                                                                                                                                                                                 
-	  WantedBy=multi-user.target      
-	 */
 
 	zpt::json _args = zpt::conf::getopt(argc, argv);
 	if (_args["add"]) {
-		std::istringstream
+		zpt::json _global_conf;
+		std::ifstream _zfs;
+		_zfs.open((std::string("/etc/zapata/zapata.conf")).data());
+		if (_zfs.is_open()) {
+			zpt::json _conf;
+			_zfs >> _conf;
+			_zfs.close();
+		}
+		
+		for (auto _to_add : _args["add"]->arr()) {
+			std::ifstream _ifs;
+			_ifs.open((std::string("/etc/zapata/backend-available/") + std::string(_to_add) + std::string(".conf")).data());
+			if (_ifs.is_open()) {
+				zpt::json _conf;
+				_ifs >> _conf;
+				_ifs.close();
+
+				_conf = _global_conf + _conf;
+				zpt::conf::setup(_conf);
+
+				if (!_conf["boot"][0]["name"]->is_string()) {
+					std::cout << "no bootable configuration found in /etc/zapata/backend-available/" << std::string(_to_add) << ".conf" << endl << flush;
+					exit(-1);
+				}
+
+				std::ofstream _ofs;
+				_ofs.open((std::string("/etc/zapata/backend-enable/") + std::string(_conf["boot"][0]["name"]) + std::string(".conf")).data(), std::ios::out | std::ios::trunc);
+				_ofs << zpt::json::pretty(_conf) << flush;
+				_ofs.close();
+
+				std::string _sysd(
+					"[Unit]\n"
+					"Description=${name}\n"
+					"${dependencies}\n"
+					"${requirements}\n"
+					"\n"
+					"[Service]\n"
+					"TimeoutStartSec=0\n"
+					"Restart=on-failure\n"
+					"RemainAfterExit=no\n"
+					"\n"
+					"ExecStart=/usr/bin/zpt -c /etc/zapata/backend-enabled/${name}.conf\n"
+					"\n"
+					"[Install]\n"
+					"WantedBy=multi-user.target\n"
+				);
+
+				std::string _after;
+				std::string _requires;
+
+				if (_conf["depends"]->is_array()) {
+					for (auto _dep : _conf["depends"]->arr()) {
+						_after += std::string("After=") + std::string(_dep) + std::string(".service\n");
+					}
+				}
+				if (_conf["requires"]->is_array()) {
+					for (auto _req : _conf["requires"]->arr()) {
+						_requires += std::string("Requires=") + std::string(_req) + std::string(".service\n");
+					}
+				}
+				zpt::replace(_sysd, "${dependencies}", _after);
+				zpt::replace(_sysd, "${requirements}", _requires);
+				zpt::replace(_sysd, "${name}", std::string(_conf["boot"][0]["name"]));
+
+				std::ofstream _sfs;
+				_sfs.open((std::string("/lib/systemd/system/") + std::string(_conf["boot"][0]["name"]) + std::string(".service")).data());
+				if (_sfs.is_open()) {
+					_sfs.close();
+				}
+				else {
+					std::cout << "couldn't write to /lib/systemd/system/" << std::string(_conf["boot"][0]["name"]) << ".service" << endl << flush;
+					exit(-1);
+				}
+			}
+			else {
+				std::cout << "no such file named /etc/zapata/backend-available/" << std::string(_to_add) << ".conf" << endl << flush;
+				exit(-1);
+			}
+		}
+						
 	}
 	else if (_args["remove"]) {
-		
+		for (auto _to_remove : _args["remove"]->arr()) {
+			if (system((std::string("rm -rf /etc/zapata/backend-enabled/") + std::string(_to_remove) + std::string(".conf")).data())) {
+			}
+			if (system((std::string("rm -rf /lib/systemd/system/") + std::string(_to_remove) + std::string(".service")).data())) {
+			}
+		}		
 	}
 	
 	return 0;
