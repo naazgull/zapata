226a10ff82b3aec1473f684f935a3804dcb0cc92
n@zgul <pedro.figueiredo@gmail.com>
2018-04-25 23:59:15 +0100

    Changed version in config files

--- smtp/include/zapata/smtp/config.h
+++ smtp/include/zapata/smtp/config.h
@@ -70,7 +70,7 @@
 #define PACKAGE_NAME "zapata-smtp"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "zapata-smtp 0.9.7-1artful58"
+#define PACKAGE_STRING "zapata-smtp 0.9.9-1artful1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "zapata-smtp"
@@ -79,10 +79,10 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.9.7-1artful58"
+#define PACKAGE_VERSION "0.9.9-1artful1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.9.7-1artful58"
+#define VERSION "0.9.9-1artful1"
