0e5e4fe4adcf09e7f4fd57af09ca2fd38eceb8d0
Matheus Santos <vorj.dux@gmail.com>
2018-07-03 14:05:40 +0100

    Improving MQTT error messages and the error logic from getting errors from mosquitto

---
+++ mqtt/include/zapata/mqtt/mqtt_utils.h
@@ -0,0 +1,29 @@
+/*
+Copyright (c) 2017, Muzzley
+*/
+
+/**
+ * This example uses the mosquitto C library (http://mosquitto.org).
+ * In Ubuntu based systems is installable by executing:
+ * $ sudo apt-get install libmosquitto0 libmosquitto0-dev
+ *
+ * Compile with '-lmosquitto'.
+ */
+
+#include <string>
+#include <zapata/json.h>
+#include <mosquitto.h>
+
+#if !defined __APPLE__
+using namespace __gnu_cxx;
+#endif
+
+namespace zpt {
+
+    namespace mqtt_utils {
+
+        auto check_err(int _return, int _errno, const std::string& _connection, zpt::LogLevel _log_level) -> int;
+
+    }
+
+}
