7b168fe53e9533473d1ddaae5b643ebe457a55f6
n@zgul <n@zgul.me>
2025-07-30 11:32:50 +0100

    Service discovery and service listing

--- common/catalog/include/zapata/catalog/catalog.h
+++ common/catalog/include/zapata/catalog/catalog.h
@@ -42,7 +42,7 @@ class catalog {
     virtual ~catalog() = default;
 
     auto clear() -> catalog&;
-    auto add(K _key, zpt::json const provider, std::uint64_t hash, M _metadata) -> catalog&;
+    auto add(K _key, zpt::json const &provider, std::uint64_t hash, M _metadata) -> catalog&;
     auto remove(K _key) -> catalog&;
     auto search(K const& _pattern) const -> zpt::json const;
     auto list() const -> zpt::json const;
@@ -92,7 +92,7 @@ auto zpt::catalog<K, M>::clear() -> catalog& {
 }
 
 template<typename K, typename M>
-auto zpt::catalog<K, M>::add(K _key, zpt::json const provider, std::uint64_t hash, M _metadata)
+auto zpt::catalog<K, M>::add(K _key, zpt::json const& provider, std::uint64_t hash, M _metadata)
   -> catalog& {
     std::ostringstream _oss;
     _oss << _key << std::flush;
@@ -179,7 +179,7 @@ template<typename K, typename M>
 auto zpt::catalog<K, M>::list() const -> zpt::json const {
     return this
       ->__catalog //
-      ->find({})
+      ->find({ "provider", "<self>" })
       ->execute()
       ->fetch();
 }
