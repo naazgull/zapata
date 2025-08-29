1204a298424fed114e46b3eaa2db4f9de3ce7444
n@zgul <n@zgul.me>
2025-08-29 18:48:58 +0100

    Adding the self protocol

---
+++ network/self/src/self.cpp
@@ -0,0 +1,61 @@
+/*
+  This is free and unencumbered software released into the public domain.
+
+  Anyone is free to copy, modify, publish, use, compile, sell, or distribute
+  this software, either in source code form or as a compiled binary, for any
+  purpose, commercial or non-commercial, and by any means.
+
+  In jurisdictions that recognize copyright laws, the author or authors of this
+  software dedicate any and all copyright interest in the software to the public
+  domain. We make this dedication for the benefit of the public at large and to
+  the detriment of our heirs and successors. We intend this dedication to be an
+  overt act of relinquishment in perpetuity of all present and future rights to
+  this software under copyright law.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <zapata/net/transport/self.h>
+#include <zapata/base.h>
+#include <zapata/globals/globals.h>
+
+auto zpt::net::transport::self::has_capability(std::uint64_t _capability) const -> bool {
+    static constexpr std::uint64_t _capabilities = zpt::transport_capability::SYNCHRONOUS;
+    return (_capabilities & _capability) == _capability;
+}
+
+auto zpt::net::transport::self::make_request() const -> zpt::message {
+    auto _to_return = zpt::allocate_message<zpt::json_message>();
+    return _to_return;
+}
+
+auto zpt::net::transport::self::make_reply(bool _with_allocator) const -> zpt::message {
+    auto _to_return = _with_allocator ? zpt::allocate_message<zpt::json_message>()
+                                      : zpt::make_message<zpt::json_message>();
+    return _to_return;
+}
+
+auto zpt::net::transport::self::make_reply(zpt::message _request) const -> zpt::message {
+    auto _to_return =
+      zpt::make_message<zpt::json_message>(zpt::message_cast<zpt::json_message>(_request), true);
+    return _to_return;
+}
+
+auto zpt::net::transport::self::process_incoming_request(zpt::stream _stream) const -> zpt::message {
+    expect(_stream->transport() == "self", "Stream underlying transport isn't 'self'");
+    auto _message = zpt::allocate_message<zpt::json_message>();
+    (*_stream) >> std::noskipws >> _message;
+    return _message;
+}
+
+auto zpt::net::transport::self::process_incoming_reply(zpt::stream _stream) const -> zpt::message {
+    expect(_stream->transport() == "self", "Stream underlying transport isn't 'self'");
+    auto _message = zpt::allocate_message<zpt::json_message>();
+    (*_stream) >> std::noskipws >> _message;
+    return _message;
+}
