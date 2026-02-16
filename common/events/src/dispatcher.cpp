a54941fe5ca0cd8a6c945d4b984cf9f3bedede92
n@zgul <n@zgul.me>
2026-02-16 18:04:26 +0000

    Debugging

--- common/events/src/dispatcher.cpp
+++ common/events/src/dispatcher.cpp
@@ -94,7 +94,6 @@ auto zpt::events::dispatcher::trap() -> dispatcher& {
         }
     }
     catch (std::exception const& _e) {
-        zlog("Uncaught exception found: " << _e.what(), zpt::error);
         if (!_event->catch_error(_e, this->shared_from_this())) {
             zlog("Uncaught exception found: " << _e.what(), zpt::error);
         }
--- engines/transport/src/engine.cpp
+++ engines/transport/src/engine.cpp
@@ -14,7 +14,8 @@ auto get_error_body(T const& _e) -> zpt::json {
 }
 
 template<typename T>
-auto report_error(T const& _e,
+auto report_error(zpt::message _received,
+                  T const& _e,
                   zpt::stream _stream,
                   zpt::polling::ptr _polling,
                   zpt::events::dispatcher::ptr _dispatcher) -> void {
@@ -24,7 +25,8 @@ auto report_error(T const& _e,
     auto _transport = zpt::TRANSPORT_LAYER() //
                         .get(_stream->transport());
 
-    if (!_transport->has_capability(zpt::transport_capability::SYNCHRONOUS) ||
+    if (_received == nullptr ||
+        !_transport->has_capability(zpt::transport_capability::SYNCHRONOUS) ||
         _polling->is_in_shutdown() || _dispatcher->is_in_shutdown()) {
         _polling->unmute(_stream);
         zlog(_e.what(), zpt::error);
@@ -32,7 +34,7 @@ auto report_error(T const& _e,
     }
 
     _stream->state() = zpt::stream_state::ERRORING_OUT;
-    auto _reply = _transport->make_reply(false);
+    auto _reply = _transport->make_reply(_received);
     _reply->status(500);
     _reply->headers()["Content-Type"] = "application/json";
     _reply->body() = ::get_error_body(_e);
@@ -74,19 +76,19 @@ auto zpt::events::receive::blocked() const -> bool { return false; }
 
 auto zpt::events::receive::catch_error(std::exception const& _e,
                                        zpt::events::dispatcher::ptr _dispatcher) -> bool {
-    ::report_error(_e, this->__stream, this->__polling, _dispatcher);
+    ::report_error(nullptr, _e, this->__stream, this->__polling, _dispatcher);
     return true;
 }
 
 auto zpt::events::receive::catch_error(std::bad_alloc const& _e,
                                        zpt::events::dispatcher::ptr _dispatcher) -> bool {
-    ::report_error(_e, this->__stream, this->__polling, _dispatcher);
+    ::report_error(nullptr, _e, this->__stream, this->__polling, _dispatcher);
     return true;
 }
 
 auto zpt::events::receive::catch_error(zpt::failed_expectation const& _e,
                                        zpt::events::dispatcher::ptr _dispatcher) -> bool {
-    ::report_error(_e, this->__stream, this->__polling, _dispatcher);
+    ::report_error(nullptr, _e, this->__stream, this->__polling, _dispatcher);
     return true;
 }
 
@@ -225,19 +227,19 @@ auto zpt::events::process::initialize(zpt::event_initialization& _init) -> void 
 
 auto zpt::events::process::catch_error(std::exception const& _e,
                                        zpt::events::dispatcher::ptr _dispatcher) -> bool {
-    ::report_error(_e, this->__stream, this->__polling, _dispatcher);
+    ::report_error(this->__received, _e, this->__stream, this->__polling, _dispatcher);
     return true;
 }
 
 auto zpt::events::process::catch_error(std::bad_alloc const& _e,
                                        zpt::events::dispatcher::ptr _dispatcher) -> bool {
-    ::report_error(_e, this->__stream, this->__polling, _dispatcher);
+    ::report_error(this->__received, _e, this->__stream, this->__polling, _dispatcher);
     return true;
 }
 
 auto zpt::events::process::catch_error(zpt::failed_expectation const& _e,
                                        zpt::events::dispatcher::ptr _dispatcher) -> bool {
-    ::report_error(_e, this->__stream, this->__polling, _dispatcher);
+    ::report_error(this->__received, _e, this->__stream, this->__polling, _dispatcher);
     return true;
 }
 
@@ -261,12 +263,10 @@ zpt::transports::engine::engine(zpt::json _config)
 #ifndef PROPAGATE_EXCEPTION
           }
           catch (std::bad_alloc const& _e) {
-              zlog(_e.what(), zpt::error);
-              ::report_error(_e, _stream, _poll, this->__dispatcher);
+              ::report_error(nullptr, _e, _stream, _poll, this->__dispatcher);
           }
           catch (std::exception const& _e) {
-              zlog(_e.what(), zpt::error);
-              ::report_error(_e, _stream, _poll, this->__dispatcher);
+              ::report_error(nullptr, _e, _stream, _poll, this->__dispatcher);
           }
 #endif
           return true;
