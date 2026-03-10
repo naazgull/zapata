b86cf8583ae9440432f1796d8cbb727c3b32f70a
n@zgul <n@zgul.me>
2026-03-10 16:38:04 +0000

    Added convertion from vector (#156)
    - Added a way to convert directly from unsigned byte array to string and back.

--- common/base/include/zapata/text/convert.h
+++ common/base/include/zapata/text/convert.h
@@ -189,6 +189,8 @@ void decode(std::string& _out);
 void encode(std::istream& _in, std::ostream& _out);
 /** @brief Decodes Base64 stream data. */
 void decode(std::istream& _in, std::ostream& _out);
+auto encode(std::vector<unsigned char> const& _in, std::string& _out) -> void;
+auto decode(std::string const& _in, std::vector<unsigned char>& _out) -> void;
 /** @brief Encodes to URL-safe Base64 (in-place). */
 void url_encode(std::string& _out);
 /** @brief Decodes URL-safe Base64 (in-place). */
