dff88a401db08645c484aa36ddc85b7794448ed8
n@zgul <n@zgul.me>
2026-03-23 22:33:18 +0000

    Muting and unmuting of streams properly serialized (#165)
    - Rearranged the order by which streams are unmuted, removed from the polling
      map and removed from the `epoll` socket.

--- io/stream/src/streams.cpp
+++ io/stream/src/streams.cpp
@@ -136,14 +136,14 @@ auto zpt::polling::unmute(zpt::stream _stream) -> zpt::polling& {
         return (*this);
     }
 
+    _stream->__muted = false;
+
     zpt::epoll_event_t _new_event;
     _new_event.events = EPOLLIN | EPOLLPRI | EPOLLERR | EPOLLHUP | EPOLLRDHUP;
     _new_event.data.ptr = _stream.get();
 
     auto _fd = static_cast<int>(*_stream);
     epoll_ctl(this->__epoll_fd, EPOLL_CTL_ADD, _fd, &_new_event);
-
-    _stream->__muted = false;
     return (*this);
 }
 
@@ -151,24 +151,24 @@ auto zpt::polling::insert(zpt::stream _stream) -> zpt::polling& {
     zpt::epoll_event_t _new_event;
     _new_event.events = EPOLLIN | EPOLLPRI | EPOLLERR | EPOLLHUP | EPOLLRDHUP;
     _new_event.data.ptr = _stream.get();
-    auto _fd = static_cast<int>(*_stream);
-    epoll_ctl(this->__epoll_fd, EPOLL_CTL_ADD, _fd, &_new_event);
-    _stream->__muted = false;
     {
         std::unique_lock _sentry{ this->__poll_lock };
-        this->__polled_streams.emplace(static_cast<int>(*_stream), _stream);
+        this->__polled_streams[static_cast<int>(*_stream)] = _stream;
     }
+    _stream->__muted = false;
+    auto _fd = static_cast<int>(*_stream);
+    epoll_ctl(this->__epoll_fd, EPOLL_CTL_ADD, _fd, &_new_event);
     return (*this);
 }
 
 auto zpt::polling::erase(zpt::stream _stream) -> zpt::polling& {
     auto _fd = static_cast<int>(*_stream);
     epoll_ctl(this->__epoll_fd, EPOLL_CTL_DEL, _fd, nullptr);
-    _stream->shutdown();
     {
         std::unique_lock _sentry{ this->__poll_lock };
         this->__polled_streams.erase(this->__polled_streams.find(_fd));
     }
+    _stream->shutdown();
     return (*this);
 }
 
