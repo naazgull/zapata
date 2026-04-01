f07213aeb5a3a3211da30588a9b810d5dc064ff0
n@zgul <n@zgul.me>
2026-04-01 18:01:57 +0100

    Rust json parser

--- bindings/rust/target/debug/libzapata_common.d
+++
@@ -1,1 +0,0 @@
-/home/pf/Develop/zapata/bindings/rust/target/debug/libzapata_common.rlib: /home/pf/Develop/zapata/bindings/rust/common/json/parser.rs /home/pf/Develop/zapata/bindings/rust/common/json.rs /home/pf/Develop/zapata/bindings/rust/common/lib.rs
