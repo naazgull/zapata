d0285215c0857bc448c923ad5c60e6230e755455
n@zgul <n@zgul.me>
2026-06-28 22:58:43 +0100

    Support PostgreSQL connector (#186)
    - Added `connector` implementation for `pgsql`.
    - Added usage examples.

---
+++ storage/pgsql/include/zapata/pgsql.h
@@ -0,0 +1,16 @@
+/*
+  This is free and unencumbered software released into the public domain.
+*/
+
+/**
+ * @file pgsql.h
+ * @brief PostgreSQL database connector.
+ *
+ * Provides PostgreSQL support using the libpq C API.
+ *
+ * @see zpt::storage::pgsql::connection
+ */
+
+#pragma once
+
+#include <zapata/pgsql/connector.h>
