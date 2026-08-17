dc8fdf5a38c41ca643dafe8deb53d750c88d9e0b
n@zgul <n@zgul.me>
2026-08-17 22:41:21 +0100

    MPMC bounded lock-free queue Vyukov's implementation
    - Simplifies lock-free queue implementation using Vyukov's algorithm for MPMC
      bounded queue.

--- common/events/src/dispatcher.cpp
+++ common/events/src/dispatcher.cpp
@@ -112,7 +112,7 @@ auto zpt::events::dispatcher::get_state() const -> zpt::json {
 }
 
 auto zpt::events::dispatcher::loop(long _consumer_nr) -> void {
-    zpt::this_thread::timer<float> _timer{ 0.005f };
+    zpt::this_thread::timer<float> _timer{ 0.01f };
     auto _name = std::format("{}@{}", this->__name, _consumer_nr);
     zpt::this_thread::name(_name);
     zlog(_name << " starting", zpt::trace);
